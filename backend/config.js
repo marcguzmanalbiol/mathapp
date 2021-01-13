658d8a850fbcb32de0011d76038cddd3f7609614
marcguzmanalbiol <marcguzmanalbiol@gmail.com>
2021-01-13 12:26:13 +0100

    API that POSTs the blog post.

--- backend/config.js
+++ backend/config.js
@@ -1,8 +1,5 @@
 const config = {
     port: process.env.PORT || 8080,
-    db: 'mongodb://localhost/mathapp',
     test_port: 4242,
-    test_db: 'mongodb://localhost/mathapp_test'
 }
-
 module.exports = config;
